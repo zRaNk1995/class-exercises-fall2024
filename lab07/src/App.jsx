4af7a10d6ce9ce6c79c8261809da46117e8ecdb7
Zach <zrank@unca.edu>
2024-11-17 10:11:33 -0400

    lab07

---
+++ lab07/src/App.jsx
@@ -0,0 +1,20 @@
+ import Profile from  "./Profile.jsx";
+ import React from "react";
+ <p>Hello React!</p>
+ <Profile />
+ <Profile />
+ <Profile />
+ <Profile />
+ export default function App() {
+
+     return (
+         <>
+             <header>
+                 <h1>My First App</h1>
+             </header>
+             <main>
+                 <p>Hello React!</p>
+             </main>
+         </>
+     );
+ }
---
+++ lab07/src/Profile.jsx
@@ -0,0 +1,10 @@
+ import "./Profile.css";
+ import React from "react";
+
+ export default function Profile() {
+     return (
+         <section className="profile">
+             Profile Goes here!
+         </section>
+     );
+ }
